28e83d062e9df505750ed7f34b489e2b8617cd85
bkarthik27599 <bkar16ec@cmrit.ac.in>
2016-09-11 18:44:51 +0530

    Update main.c

--- labprog1/main.c
+++ labprog1/main.c
@@ -1,1 +1,18 @@
-rti0
+/*prog to find roots of equation*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c,d,r1,r2,rr,ir;
+    printf("Enter the coefficients \n");
+    scanf("%f %f %f",&a,&b,&c);
+    d=(b*b)-(4*a*c);
+    if(d==0)
+          {
+            printf("roots are real and equal \n");
+            r1=-b/(2*a);
+            r2=r1;
+            printf("the roots are r1=r2= %f \n",r1);
+          }
+    if
+}
