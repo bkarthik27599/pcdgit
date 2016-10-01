d145261a9f91b9b25ef93aa043c8b04a10aedac2
bkarthik27599 <bkar16ec@cmrit.ac.in>
2016-10-01 09:31:54 +0530

    Update main.c

---
+++ labprog8/main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int search(int m, int n, char a[m] [n], char *k)
+  {
+   int left=0;
+   int right=n-1;
+   int t, mid;
+   while(left<right)
+     {
+      mid=(left+right) /2;
+      t=strcmp(k, a[mid]) ;
+      if(t>0)
+        {
+         left=mid+1;
+        } 
+      else if(t<0)
+        {
+         right=mid-1;
+        } 
+      else
+        {
+         return mid;
+        } 
+     } 
+   return - 1;
+  } 
+int main() 
+  {
+   int n,
+   
